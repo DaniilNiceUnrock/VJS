210b5d6d15c73efe30ba3f10011e7bfb5858a4cc
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-05-27 17:42:57 +0500

    Header
    Шапка готова

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,37 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  <div class="app-container">
+      <headline title ="Панель администратора">
+      <user />
+      </headline>
+  </div>
 </template>
+
+
+
+<script>
+
+import headline from "./components/headline";
+import user from "./components/user";
+
+
+export default {
+  components: {
+    headline,
+    user,
+  },
+};
+</script>
+
+
+<style lang="postcss">
+
+@import "normalize.css";
+@import "../styles/mixins.pcss";
+@import "../styles/layout/base.pcss";
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800");
+
+</style>
+
+
+
+<style lang="postcss" scoped src="./app.pcss"></style>
