f6f649937bf56e9fe29c39e7c7d18c876b50d465
DaniilNiceUnrock <84072104+DaniilNiceUnrock@users.noreply.github.com>
2021-06-04 02:21:01 +0500

    1421
    rewasrfdsf

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,58 +1,18 @@
 <template>
   <div class="app-container">
-    <headline title="Панель администрирования">
-      <user />
-    </headline>  
-    <navigation />
-    <div class="page-content">
-      <div class="container">
-        <!--<div class="header">
-          <div class="title">Блок "Обо мне"</div>
-          <iconed-button 
-          type="iconed" 
-          v-if="emptyCatIsShown === false"
-          @click="emptyCatIsShown = true" 
-          title="Добавить группу" />
-        </div>-->
-
-        
-        <!--<ul class="skills">
-          <li class="item" v-if="emptyCatIsShown">
-            <category 
-            @remove="emptyCatIsShown = false"
-            empty
-             />
-          </li>
-          <li 
-            class="item" 
-            v-for="category in categories"
-            :key="category.id">
-           <category 
-           :title="category.category"
-           :skills="category.skills"/>
-          </li>
-        </ul> -->
-        <router-view></router-view>
-      </div>
-      
-    </div>
+    <router-view name="header" />
+    <router-view />
    
   </div>
 </template>
 
 
 <script>
-import headline from "./components/headline"
-import user from "./components/user"
-import navigation from "./components/navigation"
 import button from "./components/button"
 import category from "./components/category"
 
 export default {
   components: {
-    headline,
-    user,
-    navigation,
     iconedButton: button,
     category
   },
---
+++ src/admin/pages/login/login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login-page-component">
+    <div class="content">
+        <div class="form-title">Авторизация</div>
+        <div class="row">
+          <app-input
+            title="Логин"
+            v-model="user.name"
+            icon="user"
+            :errorMessage="validation.firstError('user.name')"
+          />
+        </div>
+        <div class="row">
+          <app-input
+            title="Пароль"
+            v-model="user.password"
+            icon="key"
+            type="password"
+            :errorMessage="validation.firstError('user.password')"
+          />
+        </div>
+        <div class="btn">
+          <app-button  title="Отправить" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import appInput from "../../components/input";
+import appButton from "../../components/button";
+import { Validator, mixin as ValidatorMixin } from "simple-vue-validator";
+import axios from "axios";
+const baseUrl =  "https://webdev-api.loftschool.com";
+const token = localStorage.getItem ('token') || "";
+axios.defaults.baseURL = baseUrl;
+axios.defaults.headers['Authorization'] = `bearer ${token}`;
+
+export default {
+  mixins: [ValidatorMixin],
+  validators: {
+    "user.name": (value) => {
+      return Validator.value(value).required("Введите имя пользователя");
+    },
+    "user.password": (value) => {
+      return Validator.value(value).required("Введите пароль");
+    },
+  },
+  data: () => ({
+    user: {
+      name: "",
+      password: "",
+    },
+  }),
+  components: { appButton, appInput },
+  methods: {
+      handleSubmit() {
+          this.$validate().then((isValid) => {
+              if (isValid === false) return;
+              axios.post("")
+              console.log('request');
+          });
+      }
+  },
+};
+</script>
+
+<style lang="postcss" scoped src="./login.pcss"></style>
